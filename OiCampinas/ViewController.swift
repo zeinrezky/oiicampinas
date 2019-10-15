4ac3c084c048a459435ff474d2e4bc9dbb4f0373
Haryanto Quang <quang.haryanto@gmail.com>
2019-10-15 16:36:27 -0300

    Done with UI, brotaaaa

--- OiCampinas/ViewController.swift
+++ OiCampinas/ViewController.swift
@@ -10,12 +10,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myProject: UILabel!
+    @IBOutlet weak var myObjective: UILabel!
+    @IBOutlet weak var myMentor: UILabel!
+    
+    
+    @IBOutlet weak var btnProject: UITextField!
+    @IBOutlet weak var txtFieldObjective: UITextField!
+    @IBOutlet weak var txtFieldMentor: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
     // let's make a bit changes
+    
+    @IBAction func tapUpdateButton(_ sender: UIButton) {
+//        updateGroupMentorName(name: txtFieldMentor.text)
+//       updateLearningObjectives(add: txtFieldObjective.text)
+//       updateProjectName(projectName: btnProject.text)
+    }
+    
+    
+    
     
 }
 
